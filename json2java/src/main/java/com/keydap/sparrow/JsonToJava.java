3a93c234ed4e0c19ce667d7a59d9b280d51e9243
Kiran Ayyagari <kayyagari@keydap.com>
2016-11-05 12:33:42 +0530

    added a falg to enable/disable the resource class generation from schemas

--- json2java/src/main/java/com/keydap/sparrow/JsonToJava.java
+++ json2java/src/main/java/com/keydap/sparrow/JsonToJava.java
@@ -36,6 +36,9 @@ public class JsonToJava extends AbstractMojo {
     @Parameter
     private String baseUrl;
 
+    @Parameter
+    private boolean enabled;
+
     @Parameter(defaultValue = "${project}")
     private MavenProject project;
     
@@ -46,6 +49,11 @@ public class JsonToJava extends AbstractMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         Log log = getLog();
+
+        if(!enabled) {
+            log.info("json2java plugin is disabled, not generating resource classes.");
+            return;
+        }
 
         log.info("Starting json2java");
 
