da63c8cd38afbe1f2027ad23eeab59c88f7d24c4
Kiran Ayyagari <kayyagari@keydap.com>
2018-03-19 22:14:01 +0530

    added attributes and SAML flag based on the updated application schema

--- client/src/main/java/com/keydap/sparrow/RegisteredApp.java
+++ client/src/main/java/com/keydap/sparrow/RegisteredApp.java
@@ -1,11 +1,15 @@
 package com.keydap.sparrow;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Details of a registered application.
  *
  * @author Kiran Ayyagari (kayyagari@keydap.com)
  */
-@Resource(schemaId="urn:keydap:params:scim:schemas:core:2.0:Application", endpoint="/Applications", desc="Application")
+@Resource(schemaId = "urn:keydap:params:scim:schemas:core:2.0:Application", endpoint = "/Applications", desc = "Application")
 public class RegisteredApp {
     @ReadOnly
     private String id;
@@ -15,12 +19,14 @@ public class RegisteredApp {
     private String redirectUri;
     private boolean consentRequired;
     private boolean hasQueryInUri;
+    private List<Attribute> attributes;
+    private boolean enableSAML;
 
     @ReadOnly
     private String[] schemas;
     public static String SCHEMA = "urn:keydap:params:scim:schemas:core:2.0:Application";
 
-     public String getId() {
+    public String getId() {
         return id;
     }
 
@@ -66,5 +72,77 @@ public class RegisteredApp {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setEnableSAML(boolean enableSAML) {
+        this.enableSAML = enableSAML;
+    }
+
+    public boolean isEnableSAML() {
+        return enableSAML;
+    }
+
+    public void add(Attribute attr) {
+        if (attributes == null) {
+            attributes = new ArrayList<>();
+        }
+
+        attributes.add(attr);
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    @ComplexType(multival = true)
+    public static class Attribute implements Serializable {
+        private String name;
+        private String scimExpr;
+        private String staticVal;
+        private String staticMultiValDelim;
+
+        public Attribute() {
+        }
+
+        public Attribute(String name, String scimExpr) {
+            this.name = name;
+            this.scimExpr = scimExpr;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setScimExpr(String scimExpr) {
+            this.scimExpr = scimExpr;
+        }
+
+        public String getScimExpr() {
+            return scimExpr;
+        }
+
+        public void setStaticVal(String staticVal) {
+            this.staticVal = staticVal;
+        }
+
+        public String getStaticVal() {
+            return staticVal;
+        }
+
+        public void setStaticMultiValDelim(String staticMultiValDelim) {
+            this.staticMultiValDelim = staticMultiValDelim;
+        }
+
+        public String getStaticMultiValDelim() {
+            return staticMultiValDelim;
+        }
     }
 }
